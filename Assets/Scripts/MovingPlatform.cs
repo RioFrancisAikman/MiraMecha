ad2aae18d41ef2d4d5cfb1701880a351e22c13bc
RioFrancisAikman <115077816+RioFrancisAikman@users.noreply.github.com>
2023-04-03 16:04:40 +0100

    Created a moving platform script

---
+++ Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    public float moveTimer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveTimer += Time.deltaTime;
+
+        if (moveTimer <= 3.0f)
+        {
+            //Platform moves right
+            transform.Translate(new Vector3(1 * Time.deltaTime, 0, 0));
+
+
+
+
+        }
+        else if (moveTimer <= 6.0f)
+        {
+            //Platform moves left
+            transform.Translate(new Vector3(-1 * Time.deltaTime, 0, 0));
+
+
+        }
+        else if (moveTimer >= 6.0f)
+        {
+            // resets timer
+            moveTimer = 0;
+        }
+    }
+}
